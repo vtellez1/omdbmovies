621133258ceadc654402c8c684973d5c69ee6237
Vanessa <vanessatellez45@gmail.com>
2020-09-07 17:53:37 -0400

    updated banner position

--- src/components/banner/banner.styles.jsx
+++ src/components/banner/banner.styles.jsx
@@ -2,9 +2,15 @@ import styled from 'styled-components';
 
 export const BannerContainer = styled.div`
 padding: 1rem 2.5rem;
+
 `
 
 export const BannerDiv = styled.div`
 background-color: #800000;
 padding: 2rem;
+position: fixed;
+width: 100%;
+margin-top: 8rem;
+
+
 `
