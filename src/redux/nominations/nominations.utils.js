5ea381a18af124044c1221782791795a02c6e017
Vanessa <vanessatellez45@gmail.com>
2020-09-07 10:24:46 -0400

    removed old info, nominations state is now converted from array to object

--- src/redux/nominations/nominations.utils.js
+++ src/redux/nominations/nominations.utils.js
@@ -1,22 +1,12 @@
 export const addNominationToList = (nominationList, movieToAdd) => {
-    //return [...nominationList, movieToAdd]
 if(!(movieToAdd.imdbID in nominationList)){
     nominationList[movieToAdd.imdbID] = movieToAdd
 }
-
 return nominationList
-
 };
 
-//idea to convert to object to add:
-// if(!(movieToAdd.imdbID in nominationList)){
-//     nominationList[movieToAdd.imdbID] = movieToAdd
-// }
-
-// return nominationList
 
 export const removeNominationFromList = (nominationList, movieToRemove) => {
-    //return nominationList.filter(nominee => nominee.imdbID !== movieToRemove.imdbID)
     let removeID = movieToRemove.imdbID;
 
     const newNominationList = Object.keys(nominationList).reduce((object, key) => {
@@ -28,15 +18,3 @@ export const removeNominationFromList = (nominationList, movieToRemove) => {
 
 return newNominationList
 }
-
-//idea to convert to object to remove:
-// const removeID = movieToRemove.imdbID;
-
-// const newNominationList = Object.keys(nominationList).reduce((nominationList, key) => {
-//     if(key !== removeID){
-//         nominationList[key] = nominationList[key]
-//     }
-//     return nominationList
-// }, {})
-
-// return newNominationList
